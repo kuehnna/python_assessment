118f8b40855c09c48a4c3f7de16419a6969f7ade
Nikolas Kuehn <nikolas.a.kuehn@vanderbilt.edu>
2017-08-09 12:09:25 -0500

    Added task2 directory with script that prompts for max values and applies them. Axes will be labeled and Diagram has a title. Also included an example plot (max values of -1005 and 49.7).

--- task2/scat.py
+++ task2/scat.py
@@ -1,18 +1,20 @@
 '''
-Created on Aug 8, 2017
+Created on Aug 9, 2017
 
 @author: kuehnna
 '''
 
+
 import sys
+
 if (len(sys.argv) > 2): 
-  print "Please only use one file at a time."
+  print "Please only use one file at a time. If you were trying to provide max values as commandline arguments, please rerun without those and wait until prompted to enter them."
   exit()
 
 
 
 if (len(sys.argv) < 2):
-  file = raw_input("Please provide a sorted_models file:")
+  file = raw_input("Please provide a sorted_models file: ")
 else:
   file = sys.argv[1] 
 
@@ -26,18 +28,23 @@ maxtot = float(raw_input("Please enter maximum total_score: "))
 maxinter = float(raw_input("Please enter maximum interface_delta_X: "))
 
 
-
+#Imports DataFrame and filters based on max values provided.
 models_raw = pd.read_csv(file, sep=' ', names=['model', 'total_score', 'interface_delta_X'])
-#This is where to go on
+models = models_raw.loc[(models_raw['total_score'] <= maxtot) & (models_raw['interface_delta_X'] <= maxinter)]
 
 
-
+#Creates the plot.
 x = models['total_score']
 y = models['interface_delta_X']
 
-plt.figure()
+
+
+plt.figure(figsize=[16,9])
 plot1 = plt.scatter(x,y, s=2, c='Green', marker='.')
-plt.rcParams["figure.figsize"]= [16, 9]
+plt.tick_params(axis='both',direction='inout',width=1,length=6,labelsize=13,pad=4)
+plt.title('interface_delta_x vs total_score', size=16)
+plt.xlabel("total_score", fontsize=13)
+plt.ylabel("interface_delta_X", fontsize=13)
 plt.show()
 
 
